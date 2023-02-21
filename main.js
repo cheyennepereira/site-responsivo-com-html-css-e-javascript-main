9a86ab6564dfe702fe6bf3049fdd5922e5eb744b
cheyennepereira <56013585+cheyennepereira@users.noreply.github.com>
2023-02-21 12:41:34 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ var iconMenu = document.querySelector('nav .menu-icon img');
 
 menuBar.addEventListener('click',function(){
 
-    if (iconMenu.getAttribute("src") == 'imagens/menu.png') {
-        iconMenu.setAttribute("src","imagens/close.png");
+    if (iconMenu.getAttribute("src") == 'menu.png') {
+        iconMenu.setAttribute("src","close.png");
     }else{
-        iconMenu.setAttribute("src","imagens/menu.png");
+        iconMenu.setAttribute("src","menu.png");
     }
 
    menu.classList.toggle('active');
